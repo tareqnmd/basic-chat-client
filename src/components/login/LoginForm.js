80dee8f0d5a70487de3bbba2ff9be272f9f9bdba
Mohammad Tareq <mohammad.tareq@apsissolutions.com>
2023-03-02 14:43:22 +0600

    up

--- src/components/login/LoginForm.js
+++ src/components/login/LoginForm.js
@@ -4,14 +4,29 @@ const LoginForm = () => {
 	const [loginData, setLoginData] = useState({});
 	const loginHandler = (e) => {
 		e.preventDefault();
+		console.log(loginData);
+	};
+
+	const stateChange = (e) => {
 		const { name, value } = e.target;
 		setLoginData((prev) => ({ ...prev, [name]: value }));
-		console.log(loginData);
 	};
 	return (
 		<form className={styles.form} onSubmit={loginHandler}>
-			<input type="email" name="email" placeholder="john.doe@company.com" required />
-			<input type="password" name="password" placeholder="•••••••••" required />
+			<input
+				type="email"
+				name="email"
+				placeholder="john.doe@company.com"
+				onChange={stateChange}
+				required
+			/>
+			<input
+				type="password"
+				name="password"
+				placeholder="•••••••••"
+				onChange={stateChange}
+				required
+			/>
 			<button type="submit">Login</button>
 		</form>
 	);
