5495d3c8cc24c0e47237700ad911b4ee42c06209
tareqnmd <tareqnmd@gmail.com>
2023-03-03 11:17:45 +0600

    up

--- src/components/chats/ChatItemSend.js
+++ src/components/chats/ChatItemSend.js
@@ -1,9 +1,14 @@
+import styles from './Chats.module.scss';
+
 const ChatItemSend = () => {
-    return (
-        <>
-            
-        </>
-    );
+	return (
+		<form className={styles['chat-send']}>
+			<div className="chat-send-area">
+				<input type="text" placeholder="Comment" required />
+				<button type="submit">Send</button>
+			</div>
+		</form>
+	);
 };
 
 export default ChatItemSend;
