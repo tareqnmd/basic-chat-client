f9f0bba83495a52cd7ad4a188f1b386e67d68292
tareqnmd <tareqnmd@gmail.com>
2023-03-03 12:12:24 +0600

    up

--- src/components/chats/ChatItem.js
+++ src/components/chats/ChatItem.js
@@ -1,5 +1,20 @@
+import styles from './Chats.module.scss';
+
 const ChatItem = () => {
-	return <div>Comment</div>;
+	return (
+		<div className={styles['user-info']}>
+			<img
+				src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHw%3D&w=1000&q=80"
+				alt="user_name"
+				title="user_name"
+			/>
+			<div>
+				<strong>comment</strong>
+				<strong>comment</strong>
+				<strong>comment</strong>
+			</div>
+		</div>
+	);
 };
 
 export default ChatItem;
