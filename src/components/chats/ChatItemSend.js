eef0bb962b776dc76a0e7f7296860599ef19beb4
tareqnmd <tareqnmd@gmail.com>
2023-03-03 13:05:30 +0600

    up

--- src/components/chats/ChatItemSend.js
+++ src/components/chats/ChatItemSend.js
@@ -1,11 +1,20 @@
+import { useState } from 'react';
 import { FiSend } from 'react-icons/fi';
 import styles from './Chats.module.scss';
 const ChatItemSend = () => {
+	const [chat, setChat] = useState('');
 	return (
 		<form className={styles['chat-send']}>
 			<div className="chat-send-area">
-				<input type="text" placeholder="Comment" required />
-				<button type="submit">
+				<input
+					type="text"
+					placeholder="Comment"
+					onChange={(e) => {
+						setChat(e.target.value);
+					}}
+					required
+				/>
+				<button disabled={chat.length < 1} type="submit">
 					<FiSend />
 					<span>Send</span>
 				</button>
