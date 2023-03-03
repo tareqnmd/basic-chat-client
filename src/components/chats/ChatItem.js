bfd98de1c37b53c9f354544783f048ceabe2eb45
tareqnmd <tareqnmd@gmail.com>
2023-03-03 12:25:40 +0600

    up

--- src/components/chats/ChatItem.js
+++ src/components/chats/ChatItem.js
@@ -1,17 +1,14 @@
 import styles from './Chats.module.scss';
 
-const ChatItem = () => {
+const ChatItem = ({ chat }) => {
+	const { img, name, messages } = chat;
 	return (
-		<div className={styles['user-info']}>
-			<img
-				src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHw%3D&w=1000&q=80"
-				alt="user_name"
-				title="user_name"
-			/>
+		<div className={styles['message-details']}>
+			<img src={img} alt={name} title={name} />
 			<div>
-				<strong>comment</strong>
-				<strong>comment</strong>
-				<strong>comment</strong>
+				{messages.map((message) => (
+					<strong>{message}</strong>
+				))}
 			</div>
 		</div>
 	);
--- src/components/chats/ChatItems.js
+++ src/components/chats/ChatItems.js
@@ -1,10 +1,30 @@
 import ChatItem from './ChatItem';
 import styles from './Chats.module.scss';
 
+const chats = [
+	{
+		name: '',
+		img: 'https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHw%3D&w=1000&q=80',
+		messages: ['comment 1', 'comment 2'],
+	},
+	{
+		name: '',
+		img: 'https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80',
+		messages: ['comment 1', 'comment 2'],
+	},
+	{
+		name: '',
+		img: 'https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=400&q=60',
+		messages: ['comment 1', 'comment 2'],
+	},
+];
+
 const ChatItems = () => {
 	return (
 		<div class={styles['chat-items']}>
-			<ChatItem />
+			{chats.map((chat) => (
+				<ChatItem chat={chat} />
+			))}
 		</div>
 	);
 };
