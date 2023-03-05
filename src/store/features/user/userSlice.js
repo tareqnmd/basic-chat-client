be18641c773a34bc566bd9c30853fa17e74f2f27
Mohammad Tareq <mohammad.tareq@apsissolutions.com>
2023-03-05 19:30:41 +0600

    up

--- src/store/features/user/userSlice.js
+++ src/store/features/user/userSlice.js
@@ -8,7 +8,7 @@ const initialState = {
 	userFullName: '',
 	userEmail: '',
 	userAvatarUrl: '',
-	loading: true,
+	loading: false,
 	error: '',
 };
 
--- src/store/store.js
+++ src/store/store.js
@@ -3,7 +3,7 @@ import { applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunkMiddleware from 'redux-thunk';
 import chatReducer from './features/chats/chatSlice';
-import userReducer from './user/reducer';
+import userReducer from './features/user/userSlice';
 
 const store = configureStore(
 	{
